24985f19c79dd006b180cb69f636b9a397140f08
Ajin Alappat <105409466+AjinJAlappat@users.noreply.github.com>
2024-10-21 01:43:57 +0530

    Update Main Body.c

--- Main Body.c
+++ Main Body.c
@@ -1,19 +1,21 @@
 #include<lpc21xx.h>
 #include"lcd_fun.c"
-#define SWITCH_PIN    (1 << 10)
-#define RELAY_PIN     (1 << 12)
-#define MOISTURE_PIN  (1 << 13)
+#define SWITCH_PIN    (1 << 23)
+#define RELAY_PIN     (1 << 21)
+#define MOISTURE_PIN  (1 << 22)
 
 int main()
 {	
 	unsigned int temp_val;
 	//unsigned int moisture_value;
+	lcd_config();
 
 	IODIR0&=~SWITCH_PIN;
 	IODIR0|=RELAY_PIN;	// RELAY
+	IOCLR0=RELAY_PIN;
 	T0PR=14;
-	T0MR1=5000000; // FOR TEMPERATURE SENSOR
-	T0MCR=(1<<0)|(1<<4)|(1<<3);
+	T0MR0=5000000; // FOR TEMPERATURE SENSOR
+	T0MCR=(1<<0)|(1<<1);
 	T0TCR=(1<<1);
 	T0TCR=(1<<0);
 
@@ -22,15 +24,15 @@ int main()
 	PINSEL1&=~(1<<25);
 	 // for temperature sensor coonfiguration
 	// Configure to ADC TO select Channel 1, clk div = 4 --->0 3 MHz , Burst mode , PDN
-	ADCR=((1<<0)| (1<<1) |(4<<8)|(1<<16)|(1<<21));
+	ADCR=((1<<1) |(4<<8)|(1<<16)|(1<<21));
 
 	//For Moisture sensor
-	IODIR0|=MOISTURE_PIN; 
+	IODIR0&=~MOISTURE_PIN; 
 
 	while(1)
 	{
 		while(!(ADDR&(1<<31))); // 	MONITOR DONE BIT to checck the end of the conversion , Polling statement
-		lcd_config();
+		lcd_cmd(0x80);
 		lcd_str("Temp = ");
 		temp_val=(ADDR&(0X3FF<<6));	// Extract 10 bit digital result
 		temp_val=temp_val>>6;		   // to get as LSB
@@ -38,35 +40,48 @@ int main()
 		lcd_cmd(0x88);
 		lcd_num(temp_val);
 		delay(1000);
-
-		if(T0IR&(1<<1))
+	  /*  lcd_cmd(0xC0);
+		lcd_str("No Moisture");	  */
+		
+		if(T0IR&(1<<0))
 		{
-			if((IOPIN0 & MOISTURE_PIN)&&(temp_val<26))
+			if((IOPIN0 & MOISTURE_PIN)==1/||(temp_val<28)/)
 			{
 				IOSET0=RELAY_PIN;	 //RELAY
+	
 				lcd_cmd(0xC0);
 				lcd_str("No Moisture");
-				delay(5000);
-				IOCLR0=RELAY_PIN;
-				delay(1000);
+				//delay(5000);
+				//IOCLR0=RELAY_PIN;
+
+				
 				lcd_cmd(0x01);
+			//	delay(5000);
 			}
 			else
 			{
+				lcd_cmd(0x01);
+				lcd_cmd(0xC0);
+				lcd_str("Moisture Present");
+			//	delay(1000);
 				IOCLR0=RELAY_PIN;
 			}	
-		}
-		if(IOPIN0 & SWITCH_PIN)
-		{
-			// Switch is pressed, turn on relay
-            IOSET0 = RELAY_PIN;
-			delay(1000);
-		}
-		else 
-		{
-            // Switch is released, turn off relay
-            IOCLR0 = RELAY_PIN;
-        }	
+	
+	
+	if(IOPIN0 & SWITCH_PIN)
+	{
+		// Switch is pressed, turn on relay
+        IOSET0 = RELAY_PIN;
+		//delay(1000);
+	}
+	else 
+	{
+    // Switch is released, turn off relay
+       IOCLR0 = RELAY_PIN;
+    }
+	T0IR=(1<<0);
+	}
+	}
+//	delay(1000);	
 
-}
 }
